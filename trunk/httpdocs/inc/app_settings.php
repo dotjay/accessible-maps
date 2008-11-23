e3e79ac0c80f318ebeecbd612a01ded9f426183a
dotjay <dotjay@6df69302-8a15-11dd-aa02-45bbecb07bf0>
2008-11-23 19:40:41 +0000

    Refactored some of the JavaScript. Made map implementations a bit more flexible.

---
+++ trunk/httpdocs/inc/app_settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Accessible Maps
+ * Application Settings
+ */
+
+define('APP_NAME', 'Accessible Maps');
+
+?>
--- trunk/httpdocs/js/directions.js
+++
@@ -1,4 +0,0 @@
-function getDirections (map, containerId, fromAddress, toAddress) {
-	gdir = new GDirections(map, document.getElementById(containerId));
-	gdir.load("from: " + fromAddress + " to: " + toAddress, { "locale": en_UK });	
-}
